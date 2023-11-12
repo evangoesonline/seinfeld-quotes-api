33066ee823ac5c3bed641f0e90c9c73e37ad23fa
evangoesonline <103546051+evangoesonline@users.noreply.github.com>
2023-11-12 14:02:12 -0500

    Add basic front end placeholder for default route

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,45 @@
-// Import necessary modules
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
-// Create an Express application
 const app = express();
-const port = process.env.PORT || 3001; // You can use any port you prefer
+const port = process.env.PORT || 3001;
 
-// Middleware to enable CORS
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
-// MongoDB connection URL and database name
 const url = process.env.MONGODB_URI;
 const dbName = 'quotes';
 const collectionName = 'quote-main';
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Define a route to get data from the MongoDB collection
 app.get('/api/quotes', async (req, res) => {
     try {
-        // Connect to MongoDB
         const client = new MongoClient(url, { useUnifiedTopology: true });
         await client.connect();
 
-        // Access the quotes collection
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
-        // Query the collection for all documents
         const quotes = await collection.find({}).toArray();
 
-        // Close the connection
         await client.close();
 
-        // Send the quotes as a JSON response
         res.json(quotes);
     } catch (error) {
         console.error('Error retrieving quotes:', error);
         res.status(500).send('Internal Server Error');
     }
 });
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
 
-// Start the server
+// Define a route to serve index.html as the default file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
