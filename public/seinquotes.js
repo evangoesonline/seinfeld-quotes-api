d5b345fa50129ae6c590efe1552334de1fedccac
evangoesonline <103546051+evangoesonline@users.noreply.github.com>
2023-11-15 20:32:37 -0500

    update to use query params

--- public/seinquotes.js
+++ public/seinquotes.js
@@ -1,49 +1,44 @@
-// Define our variables
 const headerTag = document.querySelector('h2');
 const quoteTag = document.querySelector('.quote');
 let characterImage = document.querySelector('.characterImage');
+const characterSelect = document.getElementById('character');
 
-const characterImages = ["images/jerry-senfeld.png", "images/george-costanza.png", "images/elaine-benes.png", "images/cosmo-kramer.png"];
+const characterImages = {
+    Jerry: "images/jerry-senfeld.png",
+    George: "images/george-costanza.png",
+    Elaine: "images/elaine-benes.png",
+    Kramer: "images/cosmo-kramer.png"
+};
 
 const getQuote = () => {
-    fetch("/api/quotes")
+    const selectedCharacter = characterSelect.value;
+
+    // Pass the selected character as a query parameter
+    fetch(`/api/quotes?author=${selectedCharacter}`)
         .then((response) => response.json())
         .then((jsonData) => {
-            const randomIndex = Math.floor(Math.random() * jsonData.length);
-            const randomQuote = jsonData[randomIndex];
+            if (jsonData.length > 0) {
+                const randomIndex = Math.floor(Math.random() * jsonData.length);
+                const randomQuote = jsonData[randomIndex];
 
-            quoteTag.innerHTML = randomQuote.quote;
-            headerTag.innerHTML = randomQuote.author;
-            getImage();
+                quoteTag.innerHTML = randomQuote.quote;
+                headerTag.innerHTML = randomQuote.author;
+                getImage(selectedCharacter);
+            } else {
+                quoteTag.innerHTML = "No quotes available for selected character.";
+                headerTag.innerHTML = "";
+                characterImage.setAttribute("src", "images/placeholder-image.png");
+            }
         })
         .catch((error) => {
             console.error('Error fetching quotes:', error);
         });
 };
 
-// Images function
-const getImage = () => {
-    switch (headerTag.innerHTML) {
-        case "Jerry":
-            characterImage.setAttribute("src", characterImages[0]);
-            break;
-        case "George":
-            characterImage.setAttribute("src", characterImages[1]);
-            break;
-        case "Elaine":
-            characterImage.setAttribute("src", characterImages[2]);
-            break;
-        case "Kramer":
-            characterImage.setAttribute("src", characterImages[3]);
-            break;
-        default:
-            characterImage.setAttribute("src", "images/placeholder-image.png");
-    }
+const getImage = (selectedCharacter) => {
+    characterImage.setAttribute("src", characterImages[selectedCharacter] || "images/placeholder-image.png");
 };
 
 document.querySelector("button").addEventListener("click", function (event) {
     getQuote();
 });
-
-
-
